c5a3da5f6ea2e9e3992ea675e87477a2a36ddec8
Emile Bres <emile.bres@gmail.com>
2016-07-12 09:33:58 +0200

    Modify external API to expose Configuration, Grid and Store

--- source/PGridWidget/index.js
+++ source/PGridWidget/index.js
@@ -1,3 +1,6 @@
 export default from './Widget'
 
 export PivotTable from './components/Main'
+export Configuration from './components/UpperButtons'
+export Grid from './components/Grid'
+export Store from './stores/Store'
