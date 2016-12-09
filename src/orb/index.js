dbef6a6c7d79d21f080b103386d4190b378dd2e5
Emile Bres <emile.bres@gmail.com>
2016-12-09 13:53:55 +0100

    Add WrappedGrid export

--- src/orb/index.js
+++ src/orb/index.js
@@ -1,6 +1,7 @@
 import GridConfiguration from './components/GridConfiguration';
 import ChartConfiguration from './components/ChartConfiguration';
 import Grid from './components/Grid';
+import WrappedGrid from './WrappedGrid';
 import Chart from './components/Chart/Chart';
 
 import './index.css';
@@ -8,4 +9,5 @@ import './index.css';
 export { GridConfiguration };
 export { ChartConfiguration };
 export { Grid };
+export { WrappedGrid };
 export { Chart };
