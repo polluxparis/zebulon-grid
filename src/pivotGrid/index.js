6e6720b2ed6686cf7674d9dc0bbe18e8629adc33
Emile Bres <emile.bres@gmail.com>
2017-06-09 15:26:56 +0200

    Add react-app config for eslint

--- src/pivotGrid/index.js
+++ src/pivotGrid/index.js
@@ -1,22 +1,15 @@
-// import GridConfiguration from './containers/GridConfiguration';
-// import ChartConfiguration from './components/ChartConfiguration';
 import PivotGrid, { PivotGridWithoutDndContext } from './containers/PivotGrid';
 import WrappedGrid from './WrappedGrid';
-// import Chart from './components/Chart/Chart';
 import reducer from './reducers';
 import * as actions from './actions';
 import hydrateStore from './hydrateStore';
-// import './index.css';
 
 // Import polyfills
 import './utils/polyfill';
 
-// export { GridConfiguration };
-// export { ChartConfiguration };
 export default PivotGrid;
 export { PivotGridWithoutDndContext };
 export { WrappedGrid };
-// export { Chart };
 export { reducer };
 export { actions };
 export { hydrateStore };
