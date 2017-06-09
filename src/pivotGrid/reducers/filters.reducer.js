f1ff3ca386b88d2f9fd68414f109c8eef4a55fa9
Emile Bres <emile.bres@gmail.com>
2017-06-09 15:56:49 +0200

    Update react-scripts Remove umd build Fix small bug

--- src/pivotGrid/reducers/filters.reducer.js
+++ src/pivotGrid/reducers/filters.reducer.js
@@ -1,10 +1,10 @@
-import { SET_CONFIG, ADD_FILTER, DELETE_FILTER } from '../constants';
+import { SET_CONFIG_PROPERTY, ADD_FILTER, DELETE_FILTER } from '../constants';
 
 export default (state = {}, action) => {
   const { type, field, filter } = action;
   switch (type) {
     // Eventually users will be able to define filters in config
-    case SET_CONFIG:
+    case SET_CONFIG_PROPERTY:
       return state;
     case ADD_FILTER:
       return { ...state, [field]: filter };
