706e1f546668308f77070e44fd587e47d4727202
PC1\thomas <thomas.bres@gmail.com>
2017-09-25 07:07:31 +0200

    commit

--- src/pivotGrid/reducers/data.reducer.js
+++ src/pivotGrid/reducers/data.reducer.js
@@ -2,8 +2,6 @@ import { PUSH_DATA, FETCH_SUCCESS, FETCH_DATA } from "../constants";
 
 export default (state = [], action) => {
 	switch (action.type) {
-		case FETCH_DATA:
-			return [];
 		case PUSH_DATA:
 			return [...state, ...action.payload];
 		case FETCH_SUCCESS:
